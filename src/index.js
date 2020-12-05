937e519b7365016f0593f5168841f017562efd83
Aliaksandr Akhremchyk <Aliaksandr_Akhremhcyk@epam.com>
2020-12-05 21:28:39 +0300

    correct  media

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import "./script/header";
+import {
+  getDegFormLocalStorage,
+  getLengFormLocalStorage,
+} from "./script/header";
 import { getCity } from "./script/search";
 import { showTime, showDay } from "./script/time";
+import { setMap } from "./script/map";
 
 import "./index.scss";
 
 showTime();
 let time = setInterval(showTime, 1000);
+
+getDegFormLocalStorage();
+getLengFormLocalStorage();
 showDay();
-// getCity();
+setMap();
+getCity();
 
 export { time };
